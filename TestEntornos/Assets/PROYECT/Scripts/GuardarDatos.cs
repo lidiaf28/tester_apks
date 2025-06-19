9155f06bd1196ea16ece424292f8d8f80e0e13f5
aidil <aidil.simba@gmail.com>
2025-06-19 14:20:03 +0200

    Update
    Entorno configurado con ventana de usuario funcionando y ventana final.
    A falta de probar si se guarda en las gafas

---
+++ TestEntornos/Assets/PROYECT/Scripts/GuardarDatos.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using System;
+using System.IO;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+
+public class GuardarDatos : MonoBehaviour
+{
+    public Slider slider;
+    public TMP_Dropdown dropdown;
+    public GameObject ventanafinal;
+
+
+    public void GuardarDatosArchivo()
+    {
+        int valorSlider = Mathf.RoundToInt(slider.value);
+        string n_catador = dropdown.options[dropdown.value].text;
+
+        string datos = $"Número de catador: {n_catador};Intensidad de salado: {valorSlider} ";
+
+        // Generar nombre de archivo con timestamp
+        string timestamp = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+        string nombreArchivo = $"respuesta_usuario_{timestamp}.txt";
+        string ruta = Path.Combine(Application.persistentDataPath, nombreArchivo);
+
+        File.WriteAllText(ruta, datos);
+
+        string mensaje = $" Datos guardados correctamente en:\n{ruta}";
+        Debug.Log(mensaje); //mostrar mensaje en el log unity
+
+        // Abrir ventana final
+        if (ventanafinal != null)
+        {
+            ventanafinal.SetActive(true);
+        }
+    }
+}
